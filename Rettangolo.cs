7141cca099a1a48cf930b0b8b9fb37eedb8d0a1b
Nikola Krstevski <36802068+nikolaesp@users.noreply.github.com>
2022-11-25 15:28:48 +0100

    sistemato

--- Rettangolo.cs
+++ Rettangolo.cs
@@ -11,17 +11,17 @@ namespace csharp_geometria
         public int recbase;
         public int recheight;
 
-        public int calcolaArea()
+        public int CalcolaArea()
         {
-            int area = 0;
-            area = recbase * recheight;
+            
+            int area = recbase * recheight;
             return area;
         }
 
-        public int calcolaPerimetro()
+        public int CalcolaPerimetro()
         {
-            int l = 0;
-            l= (2* recbase) + (2* recheight);
+            
+            int l= (2* recbase) + (2* recheight);
             return l;
         }
 
@@ -29,8 +29,8 @@ namespace csharp_geometria
         {
             Console.WriteLine("Base: \t\t" + this.recbase+"cm.");
             Console.WriteLine("Altezza: \t" + this.recheight+"cm.");
-            Console.WriteLine("Perimetro: \t" + this.calcolaPerimetro()+"cm");
-            Console.WriteLine("Area: \t\t" + this.calcolaArea()+ "cm2");
+            Console.WriteLine("Perimetro: \t" + this.CalcolaPerimetro()+"cm");
+            Console.WriteLine("Area: \t\t" + this.CalcolaArea()+ "cm2");
         }
 
         public void DrowRectangle()
